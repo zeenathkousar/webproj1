1f23544aaebf9bc3e14b7268e05c7241d388d7c7
Zeenath Kousar <zeenathzeenath2710@gmail.com>
2022-05-26 12:31:08 +0530

    adding script start  for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const path=require("path");
 const fs=require("fs");
 const app=express();
-const port=80;
+const port=process.env.Port || 8000;
 
 //Express Specific stuff
 app.use(express.static('static'));
